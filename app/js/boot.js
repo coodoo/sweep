d9b16ef7ca8b187d793622f10c564ff3e3ab01f4
Jeremy Lu <jeremy@pubulous.com>
2015-02-25 08:19:40 +0800

    deploy

--- app/js/boot.js
+++
@@ -1,6 +0,0 @@
-﻿var MainApp = require('./views/MainApp.jsx');
-var actions = require('./actions/AppActionCreator');
-
-$(function(){
-	React.render( MainApp(), document.getElementById('container') );
-})
